8c9b88748e33d99752bc11cefe43776a8addb9e4
lpin0002 <workpinchy@gmail.com>
2025-07-11 09:59:22 +1000

    Including DM_Profile and Einasto_Profile in init dark matter module file

--- src/gammabayes/dark_matter/__init__.py
+++ src/gammabayes/dark_matter/__init__.py
@@ -2,4 +2,4 @@ from .PPPC_Tables import PPPCReader, SingleDMChannel
 # from .spectral_models import DM_ContinuousEmission_Spectrum, Z2_ScalarSinglet
 # from .combined_DM_class import CombineDMComps
 # from .custom_ratios_model import CustomDMRatiosModel
-# from .density_profiles import DM_Profile
+from .density_profiles import DM_Profile, Einasto_Profile
